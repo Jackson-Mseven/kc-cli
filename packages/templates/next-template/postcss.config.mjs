92ed1b28619d1acca1cb2f975e16d44e4c26d734
kincy <bill714@foxmail.com>
2025-05-30 11:36:59 +0800

    chore(next-template): 添加eslint import排序插件

--- packages/templates/next-template/postcss.config.mjs
+++ packages/templates/next-template/postcss.config.mjs
@@ -1,11 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    '@tailwindcss/postcss': {
-      // 可选配置项（多数项目无需修改）
-      // prefix: 'tw-',  // 自定义类名前缀
-      // important: true // 启用 !important
-    },
+    tailwindcss: {},
     autoprefixer: {}
   }
 };
